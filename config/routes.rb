9db55ec9f56ca73ec70cf9d59abb3395f6c755f6
sabrina-jones <ec2-user@ip-172-31-31-28.us-east-2.compute.internal>
2018-01-18 07:24:55 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  resources :artists, :songs, only: [:index, :show]
 
   root to: 'artists#index'
 end
